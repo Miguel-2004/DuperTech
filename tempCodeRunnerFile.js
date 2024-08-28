55387da1b1db950af7849a00b56133be2643d5df
Diego Ponce de Leon <a01664407@tec.mx>
2024-08-28 11:21:57 -0600

    Runner

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+
+    res.status(404).render('404', { pageTitle: 'Página no encontrada' });
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});

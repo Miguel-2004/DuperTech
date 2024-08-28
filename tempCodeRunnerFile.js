a6b43e02116428138861d6cbd558bd50fdb2cca1
Erick <erickeduardob@yahoo.com>
2024-08-28 11:24:40 -0600

    se elimino tempCode

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-
-    res.status(404).render('404', { pageTitle: 'Página no encontrada' });
-});
-
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});

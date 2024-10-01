fbab1720f612d5743100584e5a8f7d0a1b370e90
Erick <erickeduardob@yahoo.com>
2024-09-30 22:11:14 -0600

    modificacio en añadir empleados, ya casi esta

--- controllers/empleadoController.js
+++ controllers/empleadoController.js
@@ -23,23 +23,43 @@ exports.nuevoEmpleado = async (req, res, next) => {
         const establecimiento = await Establecimiento.getEstablecimientos();
         console.log(establecimiento);
         const admin = await Model.getAdmin();
-        const Trabajadores = await Model.getTrabajador();
+
+        //const Trabajadores = await Model.getTrabajador();
+        //const TrabajadoresArray = Array.isArray(Trabajadores) ? Trabajadores : [Trabajadores];
+        
+        // console.log(Trabajadores);
 
 
         const nombre = req.body.nombre;
         const telefono = req.body.telefono;
         const usuario = req.body.usuario;
         const contrasena = req.body.password;
-        const ID_Es = req.body.id_Establecimiento;
+        const ID_Es = req.body.ID_Establecimiento;
         const id_Admin = req.body.id_Admin;
         const empleado = await Model.createEmpleado(nombre, telefono, usuario, contrasena, ID_Es, id_Admin)
 
         if (!empleado) {
-            return res.render('empleados', { mensaje: 'Error al crear el empleado', });
+            return res.render('empleados', { mensaje: 'Error al crear el empleado' });
         }
-        res.render('empleados', { establecimiento: establecimiento });
+        res.render('empleados', { establecimiento: establecimiento, Trabajadores: TrabajadoresArray });
     } catch (e) {
         console.error(e);
         res.status(500).render('empleados', { mensaje: 'Error al cargar los datos' });
+    }
+};
+
+exports.editarEmpleado = async (req, res, next) => {
+    try{
+        const Trabajadores = await Model.getTrabajador();
+        //console.log(Trabajadores)
+        const establecimiento = await Establecimiento.getEstablecimientos();
+
+        const TrabajadoresArray = Array.isArray(Trabajadores) ? Trabajadores : [Trabajadores];
+        res.render('empleados', { Trabajadores: TrabajadoresArray, establecimiento:establecimiento });
+
+        //res.render('empleados', {Trabajadores});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error al cargar la página principal');
     }
 };
--- routes/empleado.js
+++ routes/empleado.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../controllers/empleadoController');
 
-router.get('/empleados/nuevo', Controller.nuevoEmpleado);
+router.get('/empleados/nuevo', Controller.editarEmpleado);
 router.post('/empleados/nuevo', Controller.nuevoEmpleado);
 router.get('/empleados', Controller.getAllTrabajadores);
 router.post('/empleados', Controller.getAllTrabajadores);

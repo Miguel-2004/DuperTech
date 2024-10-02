7eb7abf8ce96d569541d5b8fbb29583a6c592c9d
MIGUEL ANGEL BECERRA AYALA <111293667+Miguel-2004@users.noreply.github.com>
2024-10-02 10:19:08 -0600

    Tarjetas ubicadas en su controller

--- controllers/duper.controller.js
+++ controllers/duper.controller.js
@@ -59,32 +59,6 @@ exports.verificarUser = async (req, res, next) => {
     }
 };
 
-exports.listarTarjetas = async (req, res) => {
-    try {
-        // Obtener el ID de la sucursal del query string (para el filtrado)
-        const sucursalID = req.query.sucursalID || null;
-
-        // Obtener tarjetas, ya sea todas o filtradas por sucursal
-        const tarjetas = sucursalID
-            ? await DuperModel.getTarjetasPorSucursal(sucursalID)
-            : await DuperModel.getTarjetasConClientes();
-
-        // Obtener la lista de establecimientos
-        const establecimientos = await DuperModel.getEstablecimientos();
-
-        // Calcular el total de tarjetas
-        const totalTarjetas = tarjetas.length;
-
-        // Renderizar la vista 'tarjetas' y pasar los datos necesarios a la vista
-        res.render('tarjetas', { pageTitle: 'tarjetas', establecimientos, tarjetas, totalTarjetas });
-    } catch (error) {
-        console.error('Error al listar las tarjetas:', error);
-        res.status(500).send('Error al listar las tarjetas.');
-    }
-};
-
-
-
 //reportes
 
 // Controlador para obtener los reportes dinámicamente

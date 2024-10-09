aab45fd9299007206c039e648f8e0ac74d2b9196
Erick <erickeduardob@yahoo.com>
2024-10-09 12:54:51 -0600

    avance en editar trabajador

--- routes/empleado.js
+++ routes/empleado.js
@@ -6,12 +6,13 @@ const Controller = require('../controllers/empleadoController');
 router.get('/empleados/nuevo', Controller.editarEmpleado);
 router.post('/empleados/nuevo', Controller.nuevoEmpleado);
 
+// Editar un empleado existente
+router.get('/empleados/editar', Controller.getTrabajadores);  // Para cargar la página de edición
+router.post('/empleados/editar', Controller.editarEmpleado);     // Para enviar los datos de edición
+
 // Obtener todos los empleados
 router.get('/empleados', Controller.getTrabajadores);
 router.post('/empleados', Controller.getTrabajadores);
 
-// Editar un empleado existente
-router.get('/empleados/editar', Controller.getTrabajadores);  // Para cargar la página de edición
-router.post('/empleados/editar', Controller.editarEmpleado);     // Para enviar los datos de edición
 
 module.exports = router;
